7a3d4e63ba0c478a8a4e1dc5d26c380c5524e1d7
tieutiennu8164-hue <tieutiennu8164@gmail.com>
2026-07-13 23:08:19 +0700

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,1 +1,27 @@
-h
+const CACHE_NAME = 'app-cache-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/static/manifest.json',
+  '/static/icon.png'
+];
+
+// Kích hoạt và cài đặt Cache
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Xử lý các yêu cầu mạng (Tải từ cache nếu mất mạng)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    })
+  );
+});
